db63616d51dc42346105df1589f651fd3e4486cd
ShubhPundir <shb.pndr@gmail.com>
2025-03-19 23:57:59 +0530

    Configure Vite for portfolio deployment; set output directory to 'dist' and enable auto-opening server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/portfolio/', // the name of the github repository that I created
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    open: true,
+    port: 5173,
+  },
 })
