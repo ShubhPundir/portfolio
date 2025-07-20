77edb6139bd3f9e6244a34509c129e51b15a3553
ShubhPundir <shb.pndr@gmail.com>
2025-07-20 17:22:24 +0530

    fix: fixed contact card

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -5,9 +5,9 @@ const Contact = () => {
     <section id="contact" className="contact">
       <h2>Contact</h2>
       <p>Email: <a href="mailto:shb.pndr@gmail.com" target="_blank">shb.pndr@gmail.com</a></p>
-      <p>GitHub: <a href="https://github.com/ShubhPundir" target="_blank">@ShubhPundir</a></p>
-      <p>LinkedIn: <a href="https://www.linkedin.com/in/shubh-pundir/" target="_blank">@ShubhPundir</a></p>
-      <p>linktree: <a href="https://linktr.ee/ShubhPundir" target="_blank">@ShubhPundir</a></p>
+      <p>GitHub: <a href="https://github.com/ShubhPundir" target="_blank">Github: @ShubhPundir</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/shubh-pundir/" target="_blank">Linkedin :@ShubhPundir</a></p>
+      <p>linktree: <a href="https://linktr.ee/ShubhPundir" target="_blank">Linktree: @ShubhPundir</a></p>
     
     </section>
   );
